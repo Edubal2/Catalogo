b7226be7890286c29c33b1a26dc83773c4a3499c
jesus7vall <jesrubsan2@alu.edu.gva.es>
2025-10-30 16:01:17 +0100

    he hecho el peliculas.js y solucuinadi problemas de formularios.js

--- js/formulario.js
+++ js/formulario.js
@@ -1,24 +1,3 @@
-<form id="formulario">
-    
-    <label for="titulo">Titulo:</label>
-    <input type="text" id="titulo" name="titulo" required/>
-
-    <label for="director">Director:</label>
-    <input id="director" name="director" required></input>
-
-    <label for="año">Año de publicación</label>
-    <input type="number" id="año" name="año" required/>
-
-    <label for="genero">Género:</label>
-    <input type="text" id="genero" name="genero" required/>
-
-    <label for="valoracion">Valoración</label>
-    <input type="number" id="valoracion" name="valoracion" min="1" max="5" required/>
-
-    <button type="submit">Enviar</button>
-
-
-</form>
 
 document.getElementById("formulario").addEventListener("submit", function(event) {
     event.preventDefault();
@@ -29,3 +8,4 @@ document.getElementById("formulario").addEventListener("submit", function(event)
     const valoracion = document.getElementById("valoracion").value;
 })
 
+const nuevaPelicula = { titulo, director, año, genero, valoracion };
---
+++ js/peliculas.js
@@ -0,0 +1,24 @@
+let peliculasLista=[];
+function mostrarPeliculas(pelicula){
+    peliculasLista.push(pelicula);
+   renderizarCatalogo(); 
+}
+
+function renderizarCatalogo() {
+    const contenedor = document.getElementById("catalogo");
+    contenedor.innerHTML = "";
+
+        peliculasLista.forEach(function(pelicula) {
+            const tarjeta = document.createElement("div");
+            tarjeta.className = "tarjeta";
+                tarjeta.innerHTML = `
+                <h3>${pelicula.titulo}</h3>
+                <p><strong>Director:</strong> ${pelicula.director}</p>
+                <p><strong>Año:</strong> ${pelicula.año}</p>
+                <p><strong>Género:</strong> ${pelicula.genero}</p>
+                <p><strong>Valoración:</strong> ${pelicula.valoracion}</p>
+                `;
+            contenedor.appendChild(tarjeta);
+        });
+}
+
